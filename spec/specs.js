31f0c31044d84c280050b48e6372d264b3a23c70
Mao Thygesen <maothygesen@Maos-MacBook-Pro.local>
2016-01-19 15:17:39 -0800

    return triangle is not defined

---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('triangle', function() {
+  it("is not a triangle if two side are equal to or less than the third side", function(){
+    expect(triangle (1,1,5)).to.equal.("not a triangle")
+  })
+}
