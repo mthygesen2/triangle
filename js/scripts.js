b1c7ff7f37eaaa6a6d827d48f16fe059073ae466
Mao Thygesen <maothygesen@Maos-MacBook-Pro.local>
2016-01-19 17:18:45 -0800

    add code for isosceles triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 var triangle = function(side1, side2, side3) {
-  if(side1 === side2 && side1 === side3) {
-    return "equilateral";
-  } else {
+  if((side1 + side2 <= side3) ||
+     (side2 + side3 <= side1) ||
+     (side3 + side1 <= side2)) {
     return "not a triangle";
+  } else if((side1 === side2) && (side1 === side3)) {
+    return "equilateral";
+  } else if(((side1 === side2) && (side1 !== side3)) ||
+           ((side2 === side3) && (side2 !== side1)) ||
+           ((side3 === side1) && (side3 !== side2)) ) {
+    return "isosceles";
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,11 @@
 describe('triangle', function() {
   it("is not a triangle if two side are equal to or less than the third side", function(){
-    expect(triangle(1,1,5)).to.equal("not a triangle")
+    expect(triangle(1,2,5)).to.equal("not a triangle");
   });
   it("is a equilateral triange if all side are equal", function(){
-    expect(triangle(2,2,2)).to.equal("equilateral")
+    expect(triangle(1,1,1)).to.equal("equilateral");
+  });
+  it("is an isosceles if two side are equal", function(){
+    expect(triangle(5,5,7)).to.equal("isosceles");
   });
 });
